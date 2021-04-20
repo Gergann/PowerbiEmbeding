c2da13fda0995575b2ac178b0b3a79ba8101ddc6
Atanasov,Gergan G.A <g.atanasov@student.fontys.nl>
2021-04-20 11:40:59 +0200

    first commit

---
+++ models/embedConfigDashboard.js
@@ -0,0 +1,9 @@
+class EmbedConfigDashboard {
+    constructor(type, dashboardDetail, embedToken) {
+        this.type = type;
+        this.dashboardDetail = dashboardDetail;
+        this.embedToken = embedToken;
+    }
+}
+
+module.exports = EmbedConfigDashboard;
---
+++ models/embedDashboardConfig.js
@@ -0,0 +1,9 @@
+class PowerBiDashboardDetails {
+    constructor(dashboardId, dashboardName, embedUrl) {
+        this.dashboardId = dashboardId;
+        this.dashboardName = dashboardName;
+        this.embedUrl = embedUrl;
+    }
+}
+
+module.exports = PowerBiDashboardDetails;
